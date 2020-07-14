f4bc7c1ec9b265085db8b41d31f7f1332367f504
ahhaur <carrierlim@msn.com>
2020-07-14 10:58:14 +0800

    first commit

---
+++ HeyWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HeyWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
